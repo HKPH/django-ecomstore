cbd8b7a7f570437f32cf90918446a1890b3eac53
Phạm Huy Khôi <108706249+HKPH@users.noreply.github.com>
2024-06-11 22:53:27 +0700

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,1 @@
-import keras
 
-early_stopping_callback = keras.callbacks.EarlyStopping(
-    monitor='val_accuracy',
-    patience=5,
-    mode="max",
-    verbose=1
-)
-
-history = model.fit(
-    train_dataset, 
-    epochs=30, 
-    validation_data=valid_dataset, 
-    callbacks=[early_stopping_callback]
-)
